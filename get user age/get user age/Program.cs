b8f98bb02fabd73215c935b3ed9e0c611cdd7141
Syed Kumail Raza <136350543+syedkumailraza1@users.noreply.github.com>
2024-05-01 22:28:40 +0530

    second project

---
+++ get user age/get user age/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace get_user_age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Age is: " + age);
+            Console.ReadKey();
+        }
+    }
+}
