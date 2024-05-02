1149a7f6648031b57deb4ac4fd7dac7137f87e97
Syed Kumail Raza <136350543+syedkumailraza1@users.noreply.github.com>
2024-05-02 17:23:17 +0530

    3rd project

---
+++ Math class/Math class/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Math_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Math.Sqrt(num)
+            int num = 16;
+            Console.WriteLine("Square root of {0} is: " + Math.Sqrt(num), num);
+
+            //Math.Abs(-92)
+            int x = -92;
+            Console.WriteLine("Absolute of {0} is: " + Math.Abs(x), x);
+
+            //Math.Round()
+            double a = 9.2;
+            Console.WriteLine("The round of {0} is: " + Math.Round(a), a);
+            Console.ReadKey();
+        }
+    }
+}
