1eb6c9e22bfb5ffc9dcbf135abec4aa613745d5c
Syed Kumail Raza <136350543+syedkumailraza1@users.noreply.github.com>
2024-05-04 23:41:32 +0530

    Fouth project

---
+++ Array/Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int size;
+            Console.WriteLine("Enter the Size of array:");
+            size = int.Parse(Console.ReadLine());
+            string[] almighty= new string[size];
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Enter the Name of your friends:");
+            for (int i=0;i< size; i++)
+            {
+               almighty[i]= Console.ReadLine();
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Here is the list of your friends:");
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(almighty[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
